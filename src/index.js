fe6eeaa5f9abcbf5f52c2bb4bc43da76d8aecdab
TM <923727421@qq.com>
2020-10-08 16:55:08 +0800

    ~

---
+++ src/index.js
@@ -0,0 +1,11 @@
+"use strict";
+exports.__esModule = true;
+exports.hello = exports.test = void 0;
+function test(a) {
+    console.log("aa===", a);
+}
+exports.test = test;
+function hello(a) {
+    console.log("hello", a);
+}
+exports.hello = hello;
--- webpack.common.js
+++ webpack.common.js
@@ -2,10 +2,12 @@ const path = require("path");
 const { CleanWebpackPlugin } = require("clean-webpack-plugin");
 
 module.exports = {
-  entry: "./src/index.ts",
+  entry: path.resolve(__dirname, "src/index.ts"),
   output: {
-    filename: "[name].bundle.js",
+    filename: "index.js",
     path: path.resolve(__dirname, "dist"),
+    library: "demo",
+    libraryTarget: "umd", // 以库的形式导出入口文件时，输出的类型,这里是通过umd的方式来暴露library,适用于使用方import的方式导入npm包
   },
   module: {
     rules: [
