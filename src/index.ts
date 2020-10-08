ecdb48d027945935fdb14d35ebd0d74064f9c36c
TM <923727421@qq.com>
2020-10-08 15:32:48 +0800

    ~

---
+++ src/index.ts
@@ -0,0 +1,12 @@
+function test() {
+  console.log("test");
+}
+
+function test2() {
+  console.log("test2");
+}
+
+module.exports = {
+  test,
+  test2,
+};
---
+++ webpack.common.js
@@ -0,0 +1,23 @@
+const path = require("path");
+const { CleanWebpackPlugin } = require("clean-webpack-plugin");
+
+module.exports = {
+  entry: "./src/index.ts",
+  output: {
+    filename: "[name].bundle.js",
+    path: path.resolve(__dirname, "dist"),
+  },
+  module: {
+    rules: [
+      {
+        test: /\.tsx?$/,
+        use: "ts-loader",
+        exclude: /node_modules/,
+      },
+    ],
+  },
+  resolve: {
+    extensions: [".tsx", ".ts", ".js"],
+  },
+  plugins: [new CleanWebpackPlugin()],
+};
