8f72cb14975736a7051547dc79017a38c10766d8
TM <923727421@qq.com>
2020-10-30 13:37:00 +0800

    ~

--- webpack.common.js
+++ webpack.common.js
@@ -10,6 +10,7 @@ module.exports = {
     libraryTarget: "umd", // 以库的形式导出入口文件时，输出的类型,这里是通过umd的方式来暴露library,适用于使用方import的方式导入npm包
   },
   module: {
+    unknownContextCritical: false,
     rules: [
       {
         test: /\.tsx?$/,
